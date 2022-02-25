7ee7c9067cb543842bba0d4c0b50cb5660d4a954
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2022-02-24 22:13:08 -0300

    [u] src/resources/views/layout.blade.php, add current year in footer

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -33,7 +33,7 @@
   </main>
 
   <footer class="footer py-3">
-    <p class="text-center">Copyright @2022 | Developed by Renan Oliveira</p>
+    <p class="text-center">Copyright @<?php echo date("Y"); ?>| Developed by Renan Oliveira</p>
   </footer>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
