50b339b8786409f587bbbfe13aa1a086598ba1e0
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2022-02-24 22:02:08 -0300

    [u] src/resources/views/users/index.blade.php, btn to remove user and new layout navigator

--- src/resources/views/users/index.blade.php
+++ src/resources/views/users/index.blade.php
@@ -1,38 +1,76 @@
 @extends('layout')
 
 @section('main-content')
-  <nav class="mb-3 mt-3" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><a href="#">Home</a></li>
-    </ol>
-  </nav>
+  <div class='offset-sm-2 col-sm-8'>
+    <ul class="nav nav-tabs my-3">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+    </ul>
+  
+    <h3 class="fs-3 my-3">List Users</h3>
 
-  @if(!empty($message))
-    <div class="alert alert-success">
-        {{ $message }}
-    </div>
-  @endif
+    @if(!empty($message))
+      <div class="alert alert-success">
+          {{ $message }}
+      </div>
+    @endif
 
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($users as $user)
+    <table id="table_users" class="table table-striped">
+      <thead>
         <tr>
-          <th scope="row">{{ $user->id }}</th>
-          <td>{{ $user->first_name }}</td>
-          <td>{{ $user->last_name }}</td>
-          <td>{{ $user->email }}</td>
+          <th scope="col">ID</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Actions</th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+          <tr id="tr_user_{{ $user->id }}">
+            <th scope="row">{{ $user->id }}</th>
+            <td>{{ $user->first_name }}</td>
+            <td>{{ $user->last_name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>
+              <button
+                id="btn_destroy_user"
+                class="btn btn-danger btn-sm"
+                data-id="{{ $user->id }}" 
+                data-token="{{ csrf_token() }}" 
+              >
+                <i class="fa-solid fa-trash-can"></i>
+              </button>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
 
-  <a class="btn btn-primary" href="{{ url('users/create') }}" role="button">New User</a>
+    <a class="btn btn-primary" href="{{ url('users/create') }}" role="button">New User</a>
+  </div>
+@endsection
+
+@section('script')
+  <script type="text/javascript">
+    $("#btn_destroy_user").click(function(){
+      var id = $(this).data("id");
+      var token = $(this).data("token");
+      $.ajax({
+        url: "users/" + id,
+        type: 'DELETE',
+        dataType: "JSON",
+        data: {
+            "id": id,
+            "_method": 'DELETE',
+            "_token": token,
+        },
+        success: function (data){
+          var remove_tr_table = $("#tr_user_" + data['user_id']);
+          remove_tr_table.remove();
+        }
+      });
+    });
+  </script>
 @endsection
