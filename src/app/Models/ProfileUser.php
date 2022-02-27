ec533ec27ac1578d08a40c90752506a2fdfad814
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2022-02-27 18:22:52 -0300

    [c] src/app/Models/ProfileUser.php

---
+++ src/app/Models/ProfileUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProfileUser extends Model
+{
+    use HasFactory;
+    protected $table = 'profile_user';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class);
+    }
+}
