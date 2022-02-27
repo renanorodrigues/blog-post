3457acd4a07b2ecb8cf1e215627d1449684d4d36
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2022-02-27 18:22:17 -0300

    [u] src/app/Models/Profile.php, relationships and enum_profiles function

--- src/app/Models/Profile.php
+++ src/app/Models/Profile.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public static function enum_profiles()
+    {
+        return [
+                'Admin' => 1,
+                'Author' => 2,
+                'Subscriber' => 3,
+                'Support' => 4
+               ]; 
+    }
 }
