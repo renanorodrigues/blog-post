317227ca93bc90f8304e6239fb384bf36daa4ab9
Renan Oliveira <renan.oliveira.webdesigner@gmail.com>
2022-03-07 21:26:54 -0300

    [u] src/app/Services/LinkProfileToUserService.php, refactoring service

--- src/app/Services/LinkProfileToUserService.php
+++ src/app/Services/LinkProfileToUserService.php
@@ -5,26 +5,37 @@ namespace App\Services;
 use App\Models\ProfileUser;
 use App\Models\User;
 use App\Models\Profile;
+use DB;
 
 class LinkProfileToUserService {
   public $profile;
   public $user;
-  private $profileUser;
 
-  function __construct(Profile $profile, User $user) {
-    $this->profileUser;
+  function __construct(array $profile, User $user) {
     $this->profile = $profile;
     $this->user = $user;
   }
 
-  public function call() : bool {
-    $this->profileUser = ProfileUser::create([
-      'profile_id' => $this->profile->id,
-      'user_id' => $this->user->id
-    ]);
+  public function call() :bool {
+    if(empty($this->createProfileUser())) return false;
 
-    if($this->profileUser) return true;
+    return true;
+  }
 
-    return false;
+  private function createProfileUser() :array {
+    $profiles = [];
+
+    foreach($this->profile as $profile_id){
+      DB::transaction( function () use(&$profiles, $profile_id){
+        $profileUser = ProfileUser::create([
+          'profile_id' => $profile_id,
+          'user_id' => $this->user->id
+        ]);
+
+        array_push($profiles, $profileUser);
+      });
+    };
+
+    return $profiles;
   }
 }
